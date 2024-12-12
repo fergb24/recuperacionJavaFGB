da30497605f9924972969cf85085224725503d3f
fergb24 <fernandogbernabe@gmail.com>
2024-12-12 18:14:10 +0100

    Archivo txt con los datos y creacion de los servicios de llegadas vacio

---
+++ src/servicios/LlegadaImplementacion.java
@@ -0,0 +1,15 @@
+package servicios;
+
+public class LlegadaImplementacion implements LlegadaInterfaz {
+	
+	final char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+	
+	public boolean comprobarDni(String dni) {
+		
+		
+		
+		
+		return false;
+	}
+
+}
